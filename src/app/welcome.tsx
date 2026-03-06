7e9504469a2f1e5c2dd072c8355f80bee9d5bb87
bbroadhead <56342550+bbroadhead@users.noreply.github.com>
2026-03-06 11:19:37 -1000

    Update welcome.tsx

--- src/app/welcome.tsx
+++ src/app/welcome.tsx
@@ -27,7 +27,7 @@ const TUTORIAL_SLIDES: TutorialSlide[] = [
     id: 'welcome',
     image: (
       <Image
-        source={require('@/assets/images/TotalFlight_Icon_Resized.png')}
+        source={require('@/public/logo192.png')}
         style={{ width: 90, height: 90, resizeMode: 'contain' }}
       />
     ),
